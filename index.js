27a99744dfad3d965045f171692e9f650c9314c8
Gustavo <gustavo.viegas@brainny.cc>
2021-08-17 12:01:35 -0300

    :tada: Code file
    Express Server

---
+++ index.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+var clients = [
+    {id: 1, nome: "Gustavo Viegas", telefone: "53981125790"},
+    {id: 2, nome: "Eduardo Banana", telefone: "53912345679"},
+    {id: 3, nome: "Marcus Person", telefone: "53894545166"},
+    {id: 4, nome: "Steve Morgan", telefone: "53923165146"},
+    {id: 5, nome: "Adriano Terrera", telefone: "53965646594"},
+    {id: 6, nome: "Tranca Rua", telefone: "53971005669"},
+    {id: 7, nome: "Frango Temperado", telefone: "53561654128"},
+    {id: 8, nome: "Kai'sa Ichatia", telefone: "53231656481"}
+];
+
+app.get('/clients', (req, res) => res.json(clients));
+
+app.get('/clients/:id', (req, res) => {
+    const client = clients.filter(value => value.id == req.params.id)
+    res.json(client)
+})
+
+app.post('/clients', (req, res) =>{
+    clients.push(req.body)
+    res.send(req.body)
+})
+
+app.put('/clients/:id', (req, res) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    let client = clients.filter(value => value.id == id);
+    client[0].nome = nome;
+    res.json(client[0])
+
+})
+
+app.delete('/clients/:id', (req, res) => {
+    const id = req.params.id;
+    // Q coisa feia kkk
+    clients = clients.filter(value => value.id != id)
+    res.json(clients)
+})
+app.listen(3000)
